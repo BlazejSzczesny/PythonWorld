b784b8d12ba732dc1de5303f2f1957e80796f44b
blazej2533 <48728073+blazej2533@users.noreply.github.com>
2020-05-21 16:18:32 +0200

    Update Position.py

--- finall/Position.py
+++ finall/Position.py
@@ -9,9 +9,6 @@ class Position(object):
 		if position is not None:
 			self.__x = position.x
 			self.__y = position.y
-
-
-
 		else:
 			if xPosition is not None:
 				self.__x = xPosition
@@ -22,27 +19,20 @@ class Position(object):
 	def x(self):
 		return self.__x
 
-
 	@x.setter
 	def x(self, value):
 		self.__x = value
-
-
 
 	@property
 	def y(self):
 		return self.__y
 
-
 	@y.setter
 	def y(self, value):
 		self.__y = value
 
-
 	def __eq__(self, other):
 		return (self.x == other.x) and (self.y == other.y)
 
-
 	def __str__(self):
 		return '({0}, {1})'.format(self.x, self.y)
-
